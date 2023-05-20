b5f569af6fa14c0f76d0be0878908c84e512a7d9
royanlord <royanfarid32@gmail.com>
2023-05-20 20:57:22 +0700

    add delete reducer

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,13 +1,23 @@
+import { ADD_TODO, DELETE_TODO } from "../actions/todoAction"
+
 const initialState = {
     todos : [
-        { id: 1, title: "belajar react", isDone: false },
-        { id: 2, title: "belajar redux", isDone: false },
-        { id: 3, title: "belajar hooks", isDone: false },
+        // { id: 1, title: "belajar react", isDone: false },
+        // { id: 2, title: "belajar redux", isDone: false },
+        // { id: 3, title: "belajar hooks", isDone: false },
     ]
 }
 
 const todoReducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_TODO:
+            return {
+                todos: [...state.todos, action.payload]
+            }
+        case DELETE_TODO:
+            return {
+                todos: state.todos.filter((todo) => todo.id !== action.payload.id)
+            }
         default:
             return state
     }
