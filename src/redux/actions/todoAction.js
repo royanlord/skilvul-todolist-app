fd2230619a81a4e59def1ff9063915688527433c
royanlord <royanfarid32@gmail.com>
2023-05-20 20:56:46 +0700

    add delete action

--- src/redux/actions/todoAction.js
+++ src/redux/actions/todoAction.js
@@ -1,7 +1,19 @@
-// export const ADD_TODO = "ADD_TODO"
+export const ADD_TODO = "ADD_TODO"
+export const DELETE_TODO = "DELETE_TODO"
 
-// export const addTodo = (todo) => {
-//     return {
-//         type: ADD_TODO,
-//     }
-// }
+// let myId = 0
+
+export const addTodo = (newTodo) => {
+    // localStorage.setItem("datas", JSON.stringify(newTodo));
+    return {
+        type: ADD_TODO,
+        payload: newTodo
+    }
+}
+
+export const deleteTodo = (todo) => {
+    return {
+        type: DELETE_TODO,
+        payload: todo
+    }
+}
