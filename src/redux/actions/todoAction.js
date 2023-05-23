c9a01996e937f7973e68d65ac8466bd2fa47d07a
royanlord <royanfarid32@gmail.com>
2023-05-23 13:02:11 +0700

    create edit todo action

--- src/redux/actions/todoAction.js
+++ src/redux/actions/todoAction.js
@@ -1,19 +1,27 @@
 export const ADD_TODO = "ADD_TODO"
 export const DELETE_TODO = "DELETE_TODO"
+export const EDIT_TODO = "EDIT_TODO"
 
 // let myId = 0
 
-export const addTodo = (newTodo) => {
+export const addTodo = (todo) => {
     // localStorage.setItem("datas", JSON.stringify(newTodo));
     return {
         type: ADD_TODO,
-        payload: newTodo
+        payload: todo
     }
 }
 
 export const deleteTodo = (todo) => {
     return {
         type: DELETE_TODO,
+        payload: todo
+    }
+}
+
+export const editTodo = (todo) => {
+    return {
+        type: EDIT_TODO,
         payload: todo
     }
 }
