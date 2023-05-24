f25f5be62611cb24802995de69afa91fc5cb8528
royanlord <royanfarid32@gmail.com>
2023-05-24 23:22:23 +0700

    create filter active n completed todo action

--- src/redux/actions/todoAction.js
+++ src/redux/actions/todoAction.js
@@ -2,6 +2,8 @@ export const ADD_TODO = "ADD_TODO"
 export const DELETE_TODO = "DELETE_TODO"
 export const EDIT_TODO = "EDIT_TODO"
 export const COMPLETE_TODO = "COMPLETE_TODO"
+export const FILTER_ACTIVE_TODO = "FILTER__ACTIVE_TODO"
+export const FILTER_COMPLETE_TODO = "FILTER_COMPLETE_TODO"
 
 // let myId = 0
 
@@ -30,6 +32,20 @@ export const editTodo = (todo) => {
 export const completeTodo = (todo) => {
     return {
         type: COMPLETE_TODO,
+        payload: todo
+    }
+}
+
+export const activeFilterTodo = (todo) => {
+    return {
+        type: FILTER_ACTIVE_TODO,
+        payload: todo
+    }
+}
+
+export const completeFilterTodo = (todo) => {
+    return {
+        type: FILTER_COMPLETE_TODO,
         payload: todo
     }
 }
