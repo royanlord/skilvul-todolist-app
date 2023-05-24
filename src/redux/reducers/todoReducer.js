c74b38561c421d4eb1549389b8543b5b376bb534
royanlord <royanfarid32@gmail.com>
2023-05-24 23:22:05 +0700

    create filter active n completed todo reducer

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,32 +1,50 @@
-import { ADD_TODO, COMPLETE_TODO, DELETE_TODO, EDIT_TODO } from "../actions/todoAction"
+import { ADD_TODO, COMPLETE_TODO, DELETE_TODO, EDIT_TODO, FILTER_ACTIVE_TODO, FILTER_COMPLETE_TODO } from "../actions/todoAction"
 
 const initialState = {
     todos : [
         // { id: 1, title: "belajar react", isDone: false },
         // { id: 2, title: "belajar redux", isDone: false },
         // { id: 3, title: "belajar hooks", isDone: false },
-    ]
+    ],
+    filter: [
+        // { id: 1, title: "belajar react", isDone: false },
+        // { id: 2, title: "belajar redux", isDone: false },
+        // { id: 3, title: "belajar hooks", isDone: false },
+    ],
 }
 
 const todoReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TODO:
             return {
+                ...state,
                 todos: [...state.todos, action.payload]
             }
         case DELETE_TODO:
             return {
+                ...state,
                 todos: state.todos.filter((todo) => todo.id !== action.payload.id)
             }
         case EDIT_TODO:
             return {
-                todos: state.todos.map((todo) => todo.id === action.payload.id ? Object.assign({}, action.payload) : todo)
+                ...state,
+                todos: state.todos.map((todo) => todo.id === action.payload.id ? Object.assign(todo, action.payload) : todo)
             }
         case COMPLETE_TODO:
             return {
+                ...state,
                 todos: state.todos.map((todo) => todo.id === action.payload.id ? {...todo, isDone: !todo.isDone} : todo)
             }
-            
+        case FILTER_ACTIVE_TODO:
+            return {
+                ...state,
+                filter: state.todos.filter((todo) => !todo.isDone)
+            }
+        case FILTER_COMPLETE_TODO:
+            return {
+                ...state,
+                filter: state.todos.filter((todo) => todo.isDone)
+            }
         default:
             return state
     }
